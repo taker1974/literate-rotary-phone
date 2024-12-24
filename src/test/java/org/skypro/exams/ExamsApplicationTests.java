4b24dd4937bc1e35935ad43c363408efccf5f63c
Konstantin Terskikh <kostus.online@gmail.com>
2024-12-24 10:35:14 +0300

    Initial commit

---
+++ src/test/java/org/skypro/exams/ExamsApplicationTests.java
@@ -0,0 +1,14 @@
+package org.skypro.exams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// работа только начата
+	}
+
+}
