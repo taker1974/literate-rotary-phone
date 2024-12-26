5a655f867b3073646abaa4e1f6d8744efc593331
Konstantin Terskikh <kostus.online@gmail.com>
2024-12-26 22:39:26 +0300

    QuestionRepository: начато тестирование; начат JavaQuestionService

---
+++ src/main/java/org/skypro/exams/service/JavaQuestionService.java
@@ -0,0 +1,17 @@
+// SkyPro
+// Терских Константин, kostus.online.1974@yandex.ru, 2024
+// Курсовая работа. Java Core.
+
+package org.skypro.exams.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Сервис для работы с вопросами по Java.
+ *
+ * @author Константин Терских, kostus.online.1974@yandex.ru, 2024
+ * @version 1.1
+ */
+@Service
+public final class JavaQuestionService {
+}
